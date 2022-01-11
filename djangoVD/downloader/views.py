ee8582179dcd78eb7c9dc8a3d4dd6c34e80997a5
sauravpanchal <saurav.panchal.08@gmail.com>
2022-01-11 23:09:37 +0530

    JS Alert Update

--- djangoVD/downloader/views.py
+++ djangoVD/downloader/views.py
@@ -8,6 +8,8 @@ def home(request):
     return render(request, "index.html")
 
 def get_video(request):
+    # # return HttpResponse("<script>alert('Hii')</script>")
+    # return render(request, "index.html")
     if request.POST["inputPLATFORM"] == "YouTube":
         
         from pytube import YouTube
@@ -15,8 +17,8 @@ def get_video(request):
         link = request.POST["inputURL"]
         path = "C:\\Users\\Public"
         yt = YouTube(link)
-        # YouTube(link).streams.first().download(path)
         yt.streams.get_highest_resolution().download(path)
     else:
         return HttpResponse("<h1>Twitter Downloads will be available soon !</h1>")
-    return HttpResponse("<h1>Video Downloaded, Successfully !</h1> <br> <h3>Path: C:\\Users\\Public</h3>")
+    # return HttpResponse("<h1>Video Downloaded, Successfully !</h1> <br> <h3>Path: C:\\Users\\Public</h3>")
+    return render(request, "get_video.html")
