035329b2863f493167570e7d0b814e66b57ba066
sauravpanchal <saurav.panchal.08@gmail.com>
2022-01-18 18:40:43 +0530

    Deploy

--- djangoVD/djangoVD/settings.py
+++ djangoVD/djangoVD/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+django_heroku.settings(locals())
